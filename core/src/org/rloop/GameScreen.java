b839ae27775002dd3f6964d2f2f4ab1461e2e36b
Daniil-Y <daniil.yurshevich@student.uj.edu.pl>
2022-05-01 13:45:38 +0200

    for pulling

--- core/src/org/rloop/GameScreen.java
+++ core/src/org/rloop/GameScreen.java
@@ -1,6 +1,12 @@
 package org.rloop;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.ScreenAdapter;
 
 public class GameScreen extends ScreenAdapter {
+    final Game game;
+
+    public GameScreen(Game game) {
+        this.game = game;
+    }
 }
