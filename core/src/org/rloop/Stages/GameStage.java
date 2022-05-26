9150a9d77bacfd57ad223a4bbb73f0fa93055317
leondorus <mrbelarusman@gmail.com>
2022-05-26 04:52:14 +0200

    Temporally removed statWindow and statHeader

--- core/src/org/rloop/Stages/GameStage.java
+++ core/src/org/rloop/Stages/GameStage.java
@@ -11,7 +11,7 @@ public class GameStage extends Stage {
     Stage currentStage;
     public Stage getCurrentStage() {return currentStage;}
     ProgressBar healthBar;
-    Window statWindow;
+//    Window statWindow;
 
     public GameStage(final GameScreen gameScreenSuper, Skin skin) {
         currentStage = new Stage(new ScreenViewport());
@@ -24,17 +24,17 @@ public class GameStage extends Stage {
         healthBar.setHeight(Util.monitorResolutionY(50));
         currentStage.addActor(healthBar);
 
-        statWindow = new Window("", skin);
-        statWindow.setResizable(false);
-        statWindow.setBounds(Util.monitorResolutionX(30), Util.monitorResolutionY(1040 - (int)healthBar.getHeight() - 420), Util.monitorResolutionX(300), Util.monitorResolutionY(400));
-        currentStage.addActor(statWindow);
+//        statWindow = new Window("", skin);
+//        statWindow.setResizable(false);
+//        statWindow.setBounds(Util.monitorResolutionX(30), Util.monitorResolutionY(1040 - (int)healthBar.getHeight() - 420), Util.monitorResolutionX(300), Util.monitorResolutionY(400));
+//        currentStage.addActor(statWindow);
 
-        Label statHeader = new Label("Stats", skin);
-        statHeader.setWidth(Util.monitorResolutionX((int)statHeader.getWidth()));
-        statHeader.setHeight(Util.monitorResolutionY((int)statHeader.getHeight()));
-        statHeader.setFontScale(Gdx.graphics.getWidth() * 1.0f/1920, Gdx.graphics.getHeight() * 1.0f/1080);
-        statHeader.setPosition(Util.monitorResolutionX((int) (30 + 300/ 2 - statHeader.getWidth()/2)), Util.monitorResolutionY((int) (1040 - (int)healthBar.getHeight() - 40 - statHeader.getHeight())));
-        currentStage.addActor(statHeader);
+//        Label statHeader = new Label("Stats", skin);
+//        statHeader.setWidth(Util.monitorResolutionX((int)statHeader.getWidth()));
+//        statHeader.setHeight(Util.monitorResolutionY((int)statHeader.getHeight()));
+//        statHeader.setFontScale(Gdx.graphics.getWidth() * 1.0f/1920, Gdx.graphics.getHeight() * 1.0f/1080);
+//        statHeader.setPosition(Util.monitorResolutionX((int) (30 + 300/ 2 - statHeader.getWidth()/2)), Util.monitorResolutionY((int) (1040 - (int)healthBar.getHeight() - 40 - statHeader.getHeight())));
+//        currentStage.addActor(statHeader);
 
         Label hpHeader = new Label("HP", skin);
         hpHeader.setColor(1f,1f,1f,1f);
